b0c6e81b1e19449e38ef8747a65ce845a759eccd
Tuomo Hänninen <tuomo.hanninen@myy.haaga-helia.fi>
2026-01-03 10:48:13 +0200

    Changed message again.

--- express-app/index.js
+++ express-app/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hoi maailma!</h1>')
+  res.send('<h1>Hoi vaan taas maailma!</h1>')
 })
 
 const PORT = 8080
